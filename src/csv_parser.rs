0c1967b1f0985c1164d0e580d4a2911dd3ba13c0
Jakub Stibůrek <jakub@stiburek.dev>
2023-08-15 11:04:53 +0200

    refactor: move csv parsing to new module, introduce prelude module

---
+++ src/csv_parser.rs
@@ -0,0 +1,58 @@
+use crate::prelude::*;
+
+pub fn process_csv_file(file_path: &Path) -> Result<(), Box<dyn Error>> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let mut csv_reader = ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(contents.as_bytes());
+
+    let name = file_path.file_stem().unwrap().to_str().unwrap().to_string();
+    let headers = csv_reader.headers()?.iter().map(|s| s.to_string()).collect();
+    let mut records: Vec<Vec<String>> = vec![];
+
+    for record in csv_reader.records() {
+        let record = record?;
+        let record_strings: Vec<String> = record.iter().map(|field| field.to_string()).collect();
+        records.push(record_strings);
+    }
+
+    create_table(&name, &headers, &records)?;
+
+    Ok(())
+}
+
+fn create_table(name: &String, headers: &Vec<String>, records: &Vec<Vec<String>>) -> Result<(), Box<dyn Error>> {
+    println!("Creating table {}...", name);
+
+    let mut file = File::create(format!("{}.sql", name))?;
+
+    let mut sql = format!("CREATE TABLE {} (", name);
+
+    let columns = headers.iter().map(|s| format!("{} TEXT", s)).collect::<Vec<String>>().join(", ");
+
+    sql.push_str(&columns);
+
+    sql.push_str(");");
+
+    for record in records {
+        let mut sql_record = format!("INSERT INTO {} VALUES (", name);
+
+        let values = record.iter().map(|s| format!("'{}'", s)).collect::<Vec<String>>().join(", ");
+
+        sql_record.push_str(&values);
+
+        sql_record.push_str(");");
+
+        sql.push_str("\n");
+        sql.push_str(&sql_record);
+    }
+
+    file.write_all(sql.as_bytes())?;
+
+    println!("Table {} created. 🚀", name.on_green());
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use clap::Parser;
-use std::{fs, path::Path, error::Error};
-use std::fs::File;
-use std::io::{Read, Write};
-use csv::ReaderBuilder;
-use colored::*;
+mod csv_parser;
+
+mod prelude {
+    pub use clap::Parser;
+    pub use std::{fs, path::Path, error::Error};
+    pub use std::fs::File;
+    pub use std::io::{Read, Write};
+    pub use csv::ReaderBuilder;
+    pub use colored::*;
+    pub use crate::csv_parser::*;
+}
+
+use prelude::*;
+
 
 #[derive(Parser, Debug)]
 #[command(author = "Jakub Stibůrek", version = "0.1.2", about = "CSV -> SQL table")]
@@ -74,59 +82,4 @@ fn validate_args(args: &Args) -> Result<Vec<String>, (Vec<String>, Vec<String>)>
     return Ok(ok_paths);
 }
 
-fn process_csv_file(file_path: &Path) -> Result<(), Box<dyn Error>> {
-    let mut file = File::open(file_path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
 
-    let mut csv_reader = ReaderBuilder::new()
-        .has_headers(true)
-        .from_reader(contents.as_bytes());
-
-    let name = file_path.file_stem().unwrap().to_str().unwrap().to_string();
-    let headers = csv_reader.headers()?.iter().map(|s| s.to_string()).collect();
-    let mut records: Vec<Vec<String>> = vec![];
-
-    for record in csv_reader.records() {
-        let record = record?;
-        let record_strings: Vec<String> = record.iter().map(|field| field.to_string()).collect();
-        records.push(record_strings);
-    }
-
-    create_table(&name, &headers, &records)?;
-
-    Ok(())
-}
-
-fn create_table(name: &String, headers: &Vec<String>, records: &Vec<Vec<String>>) -> Result<(), Box<dyn Error>> {
-    println!("Creating table {}...", name);
-
-    let mut file = File::create(format!("{}.sql", name))?;
-
-    let mut sql = format!("CREATE TABLE {} (", name);
-
-    let columns = headers.iter().map(|s| format!("{} TEXT", s)).collect::<Vec<String>>().join(", ");
-
-    sql.push_str(&columns);
-
-    sql.push_str(");");
-
-    for record in records {
-        let mut sql_record = format!("INSERT INTO {} VALUES (", name);
-
-        let values = record.iter().map(|s| format!("'{}'", s)).collect::<Vec<String>>().join(", ");
-
-        sql_record.push_str(&values);
-
-        sql_record.push_str(");");
-
-        sql.push_str("\n");
-        sql.push_str(&sql_record);
-    }
-
-    file.write_all(sql.as_bytes())?;
-
-    println!("Table {} created. 🚀", name.on_green());
-
-    Ok(())
-}
